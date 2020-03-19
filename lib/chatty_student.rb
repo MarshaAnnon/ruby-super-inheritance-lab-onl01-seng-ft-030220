1d8666df09c121f0a744271acf7941b33371d269
Marsha Annon <marshaannon@gmail.com>
2020-03-19 17:42:21 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,9 +3,7 @@ require "pry"
 class ChattyStudent
 
   def hello
-    "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind
- of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just t
-ell you who died..."
+    "Hey there! I'm so excited to learn stuff."
   end
 
 end
